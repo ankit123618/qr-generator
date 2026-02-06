914a445f3b0ff15d7490fc4181b42b16d12888b8
ankit123618 <as4808385@gmail.com>
2026-02-06 14:38:20 +0530

    yes

--- script.js
+++ script.js
@@ -10,19 +10,18 @@ function generateQR() {
     qrCanvas = null;
 
     if (!url) {
-        alert("Please enter a URL");
+        alert("Enter a URL");
         return;
     }
 
-    // Force valid URL
     if (!url.startsWith("http://") && !url.startsWith("https://")) {
         url = "https://" + url;
     }
 
-    // Quiet zone wrapper
+    // Bigger quiet zone for watches
     const wrapper = document.createElement("div");
     wrapper.style.background = "white";
-    wrapper.style.padding = "12px";
+    wrapper.style.padding = "20px";
     wrapper.style.display = "inline-block";
 
     qrBox.appendChild(wrapper);
@@ -32,27 +31,26 @@ function generateQR() {
 
     new QRCode(qrDiv, {
         text: url,
-        width: 256,
-        height: 256,
+        width: 100,
+        height: 100,
         colorDark: "#000000",
         colorLight: "#ffffff",
         correctLevel: QRCode.CorrectLevel.H
     });
 
-    // Wait for QR render
     setTimeout(() => {
         qrCanvas = qrDiv.querySelector("canvas");
         if (qrCanvas) {
             downloadBtn.style.display = "inline-block";
         }
-    }, 300);
+    }, 400);
 }
 
 function downloadQR() {
     if (!qrCanvas) return;
 
     const link = document.createElement("a");
-    link.download = "qr-code.png";
+    link.download = "watch-qr.png";
     link.href = qrCanvas.toDataURL("image/png");
     link.click();
 }
